8d3faa1559be7c0e9b61176fb056ced895400da8
JHPDEVS <jhpdevs@g.yju.ac.kr>
2022-04-21 10:55:54 +0900

    Initial commit

---
+++ app/src/main/java/com/wdj/mankai/parser/JSONParser.java
@@ -0,0 +1,12 @@
+package com.wdj.mankai.parser;
+
+import org.json.JSONObject;
+
+import java.io.InputStream;
+
+public class JSONParser {
+
+    static InputStream inputStream = null;
+    static JSONObject jObj = null;
+    static String json = "";
+}
