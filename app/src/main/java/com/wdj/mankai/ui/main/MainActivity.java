e18a85dab3d158c10dd0c7f3f55a31de0e80bac5
JHPDEVS <jhpdevs@g.yju.ac.kr>
2022-04-25 20:44:06 +0900

    Add Bottom Navigate View

--- app/src/main/java/com/wdj/mankai/ui/main/MainActivity.java
+++ app/src/main/java/com/wdj/mankai/ui/main/MainActivity.java
@@ -1,22 +1,28 @@
 package com.wdj.mankai.ui.main;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.toolbox.Volley;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.wdj.mankai.R;
-import com.wdj.mankai.ui.login.LoginActivity;
-import com.wdj.mankai.ui.login.LoginRequest;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.security.acl.Group;
+
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +31,33 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences sharedPreferences= getSharedPreferences("login_token", MODE_PRIVATE);
         String token = sharedPreferences.getString("login_token","");
         getUser(token); // 유저 정보 받아오기
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_container,new HomeFragment()).commit();
+
+        bottomNavigationView.setSelectedItemId(R.id.home);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.chat:
+                        fragment = new ChatFragment();
+                        break;
+                    case R.id.group:
+                        fragment = new GroupFragment();
+                        break;
+                    case R.id.mypage:
+                        fragment = new MyPageFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_container,fragment).commit();
+                return true;
+            }
+        });
     }
 
     private void getUser(String token) {
